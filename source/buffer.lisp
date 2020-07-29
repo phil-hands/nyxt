2408f3b2704aab60138ed35d0ad468e734119e17
John Mercouris <john@mercouris.email>
2020-07-29 16:15:08 +0200

    buffer.lisp: create internal-buffer class

--- source/buffer.lisp
+++ source/buffer.lisp
@@ -238,6 +238,9 @@ The handlers take the buffer as argument.")
 Must be one of `:always' (accept all cookies), `:never' (reject all cookies),
 `:no-third-party' (accept cookies for current website only).")))
 
+(defclass internal-buffer (buffer) ()
+  (:documentation "A buffer with the ability to run lisp:// codified URLs."))
+
 (defmethod proxy ((buffer buffer))
   (slot-value buffer 'proxy))
 
